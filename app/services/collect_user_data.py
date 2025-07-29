36e9b19de9f67ea76712386aa29fb7e9c14f93ff
hayong39 <yia0524_hy@naver.com>
2025-07-29 19:56:37 +0900

    [UBLE-93] fix: 최근 3개월치 로그 조회하도록 변경

--- app/services/collect_user_data.py
+++ app/services/collect_user_data.py
@@ -3,6 +3,7 @@ from sqlalchemy import text
 from elasticsearch import Elasticsearch
 from elasticsearch.helpers import scan
 import logging
+from datetime import datetime, timedelta
 
 logger = logging.getLogger(__name__)
 
@@ -15,25 +16,44 @@ def collect_user_data(user_id: int, db: Session, es: Elasticsearch) -> tuple[lis
         WHERE uc.user_id = :user_id
     """), {"user_id": user_id}).scalars().all()
 
-    histories = db.execute(text("""
-        SELECT s.name FROM usage_history uh
-        JOIN store s ON uh.store_id = s.id
-        WHERE uh.user_id = :user_id
-    """), {"user_id": user_id}).scalars().all()
-
     bookmarks = db.execute(text("""
         SELECT b.description FROM bookmark bm
         JOIN brand b ON bm.brand_id = b.id
         WHERE bm.user_id = :user_id
     """), {"user_id": user_id}).scalars().all()
 
+    three_months_ago = (datetime.utcnow() - timedelta(days=90)).isoformat()
+
+    # es에서 가져오는 이용 내역 로그
+    histories = []
+    try:
+        for doc in scan(es, index="usage-history-log", query={
+            "query": {
+                "bool": {
+                    "must": [
+                        {"term": { "userId": user_id }},
+                        {"range": { "createdAt": {"gte": three_months_ago}}}
+                    ]
+                }
+            }
+
+        }):
+            store_name = doc["_source"].get("storeName")
+            if store_name:
+                histories.append(store_name)
+    except Exception as e:
+        logger.error(f"이용내역 로그 조회 실패 (user_id: {user_id}: {e})")
+
     # es에서 가져오는 클릭 로그
     clicks = []
     try:
         for doc in scan(es, index="store-click-log", query={
             "query": {
-                "term": {
-                    "userId": user_id
+                "bool": {
+                    "must": [
+                        {"term": { "userId": user_id }},
+                        {"range": { "createdAt": {"gte": three_months_ago}}}
+                    ]
                 }
             }
         }):
@@ -48,8 +68,11 @@ def collect_user_data(user_id: int, db: Session, es: Elasticsearch) -> tuple[lis
     try:
         for doc in scan(es, index="search-log", query={
             "query": {
-                "term": {
-                    "userId": user_id
+                "bool": {
+                    "must": [
+                        {"term": { "userId": user_id }},
+                        {"range": { "createdAt": {"gte": three_months_ago}}}
+                    ]
                 }
             }       
         }):
@@ -58,4 +81,5 @@ def collect_user_data(user_id: int, db: Session, es: Elasticsearch) -> tuple[lis
                 searches.append(keyword)
     except Exception as e:
         logger.error(f"검색 로그 조회 실패 (user_id: {user_id}: {e})")
+
     return categories, histories, bookmarks, clicks, searches
